bbc2772b9d098e2543661a8cc84ad204927e4214
YukhnikOlga <olga.iukhnik@gmail.com>
2024-05-26 14:49:16 +0300

    more tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,7 +12,28 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMiddleMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 150, 8, 6, 11, 11, 12};
+        long expected = 150;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findFinishMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 111};
+        long expected = 111;
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
